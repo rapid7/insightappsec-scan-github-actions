1b7aa78a6c7688037c66bda2c38b3967cb3e8d3f
Sophie Stephenson <sophie_stephenson@rapid7.com>
2022-03-01 11:32:39 +0000

    Code review changes

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function isInputValid(key, value) {
 }
 
 async function performAction() {
-    const region = core.getInput(INPUT_REGION);
-    const apiKey = core.getInput(INPUT_API_KEY);
-    const scanConfigId = core.getInput(INPUT_SCAN_CONFIG_ID);
-    const vulnQuery = core.getInput(INPUT_VULN_QUERY) || null;
-    const waitScanComplete = core.getBooleanInput(INPUT_WAIT_SCAN_COMPLETE);
-    let scanTimeoutMins = core.getInput(INPUT_SCAN_TIMEOUT_MINS) || null;
+    const region = 'US'; //core.getInput(INPUT_REGION);
+    const apiKey = '468b7fe4-213b-4b47-93e2-db96db45a7a0'; //core.getInput(INPUT_API_KEY);
+    const scanConfigId = "89170081-7db0-4eaf-acce-563c9adfe3c2"; //core.getInput(INPUT_SCAN_CONFIG_ID);
+    const vulnQuery = null; //core.getInput(INPUT_VULN_QUERY) || null;
+    const waitScanComplete = true; //core.getBooleanInput(INPUT_WAIT_SCAN_COMPLETE);
+    let scanTimeoutMins = null; //core.getInput(INPUT_SCAN_TIMEOUT_MINS) || null;
 
     if(scanTimeoutMins) {
         try{
@@ -66,7 +66,7 @@ async function performAction() {
 
             if(success) {
                 const result = await scanTools.getScanResultsSummary(scanId, vulnQuery, scanConfigId);
-                core.setOutput(OUTPUT_SCAN_FINDINGS, JSON.stringify({vulnerabilities: result[0], scanLink: result[1]}, null, 2));
+                core.setOutput(OUTPUT_SCAN_FINDINGS, JSON.stringify({vulnerabilities: result.vulnerabilities, scanLink: result.scanLink}, null, 2));
                 if (Object.keys(result).length != 0 && vulnQuery) {
                     core.setFailed("Vulnerabilities were found in scan. Failing.");
                 }
