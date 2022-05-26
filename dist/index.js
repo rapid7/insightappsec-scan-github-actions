4abf56bf6a32b28a550b286d1b102cfff2088366
Sophie Stephenson <sophie_stephenson@rapid7.com>
2022-05-26 09:58:44 +0100

    Testing invalid Scan Config ID

--- dist/index.js
+++ dist/index.js
@@ -6137,7 +6137,7 @@ function isInputValid(key, value) {
 async function performAction() {
     const region = core.getInput(INPUT_REGION);
     const apiKey = core.getInput(INPUT_API_KEY);
-    const scanConfigId = core.getInput(INPUT_SCAN_CONFIG_ID);
+    const scanConfigId = "fakeScanConfigID"; //core.getInput(INPUT_SCAN_CONFIG_ID);
     const vulnQuery = core.getInput(INPUT_VULN_QUERY) || null;
     const waitScanComplete = core.getBooleanInput(INPUT_WAIT_SCAN_COMPLETE);
     let scanTimeoutMins = core.getInput(INPUT_SCAN_TIMEOUT_MINS) || null;
