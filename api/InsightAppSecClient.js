b05ebe464d19a1fdd224a619a4e57ada1d03f12a
nmorrissey-r7 <106983623+nmorrissey-r7@users.noreply.github.com>
2026-08-12 10:18:26 +0100

    chore: bump version to 1.5.1

--- api/InsightAppSecClient.js
+++ api/InsightAppSecClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const APPLICATION_JSON = "application/json";
-const USER_AGENT_HEADER = "r7:insightappsec-github-actions/1.5.0";
+const USER_AGENT_HEADER = "r7:insightappsec-github-actions/1.5.1";
 
 // Base URLs held as complete literals, deliberately NOT built by interpolating
 // the region into a template string. region arrives from the consuming workflow's
